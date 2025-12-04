af75b671573375be846f52b2708cfcf8c3bea7d0
datamarathon <datamarathonhiring@gmail.com>
2025-12-04 10:14:48 +0530

    made api changes

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -10,7 +10,7 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "netflixgpt-a74a2.firebaseapp.com",
   projectId: "netflixgpt-a74a2",
   storageBucket: "netflixgpt-a74a2.firebasestorage.app",
